cc57cd1008f24628d1bb448f8110c3edf8fe34ff
Tanuj upadhyay <upadhyaytanuj123@gmail.com>
2020-07-07 12:05:02 +0530

    First commit

---
+++ ProjectTracker/src/main/java/com/alphacode/ppmtool/ProjectTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.alphacode.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTrackerApplication.class, args);
+	}
+
+}
