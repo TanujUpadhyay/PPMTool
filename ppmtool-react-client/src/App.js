923994364f8a5fb2463d3a4a2a42d612eb8a2bfb
Tanuj upadhyay <upadhyaytanuj123@gmail.com>
2020-07-09 10:28:11 +0530

    adding dashboard and projectiteam components and also adding bootstrap , fontawsome

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import DashBoard from "./components/DashBorad";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <DashBoard />
+      <Header />
       <DashBoard />
     </div>
   );
--- ppmtool-react-client/src/components/DashBorad.js
+++ ppmtool-react-client/src/components/DashBorad.js
@@ -1,9 +1,23 @@
 import React from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 function DashBoard() {
   return (
-    <div>
-      <h1>Welcome to the dashborad</h1>
+    <div className="projects">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="display-4 text-center">Projects</h1>
+            <br />
+            <a href="ProjectForm.html" className="btn btn-lg btn-info">
+              Create a Project
+            </a>
+            <br />
+            <hr />
+          </div>
+          <ProjectItem />
+        </div>
+      </div>
     </div>
   );
 }
