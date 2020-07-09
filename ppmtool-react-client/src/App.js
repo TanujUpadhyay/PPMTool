f1a21d8785790b3094ecea2d3edf72314728569a
Tanuj upadhyay <upadhyaytanuj123@gmail.com>
2020-07-09 09:31:19 +0530

    Creating react project and make first comopent

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DashBoard from "./components/DashBorad";
+
+function App() {
+  return (
+    <div className="App">
+      <DashBoard />
+      <DashBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ ppmtool-react-client/src/components/DashBorad.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DashBoard() {
+  return (
+    <div>
+      <h1>Welcome to the dashborad</h1>
+    </div>
+  );
+}
+
+export default DashBoard;
