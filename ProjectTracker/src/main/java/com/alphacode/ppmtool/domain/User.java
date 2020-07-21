26860ec43cb6c3f5a237fd8a94c81ba3c1b1a7b5
Tanuj upadhyay <upadhyaytanuj123@gmail.com>
2020-07-21 08:33:04 +0530

    ading jwt and spring secutrity jars and making some custome exception for handling the invalid user error and also add some class for security

---
+++ ProjectTracker/src/main/java/com/alphacode/ppmtool/domain/User.java
@@ -0,0 +1,5 @@
+package com.alphacode.ppmtool.domain;
+
+public class User {
+
+}
---
+++ ProjectTracker/src/main/java/com/alphacode/ppmtool/web/UserController.java
@@ -0,0 +1,8 @@
+package com.alphacode.ppmtool.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
